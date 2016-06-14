c581ae82c56ab3b1fb08e560c0e8311e7a2f3ca5
Brian Woodward <brian.woodward@gmail.com>
2016-06-14 16:58:40 -0400

    initial implementation and tests

--- index.js
+++ index.js
@@ -7,15 +7,113 @@
 
 'use strict';
 
-var debug = require('debug')('base-reporter');
+var Base = require('base');
+var isValid = require('is-valid-app');
+var extend = require('extend-shallow');
 
-module.exports = function(config) {
-  return function(app) {
-    if (this.isRegistered('base-reporter')) return;
+/**
+ * Exposes smart plugin for adding a `reporter` instance to a base application.
+ *
+ * ```js
+ * app.use(reporter());
+ * ```
+ * @param  {Object} `config` Configuration object.
+ * @return {Function} Plugin function to use with `app.use`
+ * @api public
+ */
 
-    this.define('base-reporter', function() {
-      debug('running base-reporter');
-      
-    });
+module.exports = function reporter(config) {
+  return function plugin(app) {
+    if (!isValid(this, 'base-reporter')) return;
+
+    var options = extend({}, config);
+    this.define('reporter', new Reporter(this, options));
   };
 };
+
+
+/**
+ * Reporter class for registering reporters, gathering information and running reports.
+ *
+ * ```js
+ * var reporter = new Reporter(app);
+ * ```
+ * @param {Object} `app` base application instance
+ * @param {Object} `options` Additional options for reporters to use.
+ */
+
+function Reporter(app, options) {
+  if (!(this instanceof Reporter)) {
+    return new Reporter(app, options);
+  }
+  Base.call(this, {}, options);
+  this.define('app', app);
+}
+
+/**
+ * Extend Base
+ */
+
+Base.extend(Reporter);
+
+/**
+ * Capture file paths going through a middleware.
+ *
+ * ```js
+ * app.preWrite(/./, app.reporter.captureFiles());
+ * ```
+ * @name reporter.captureFiles
+ * @return {Function} Function that can be used as a middleware function.
+ * @api public
+ */
+
+Reporter.prototype.captureFiles = function() {
+  var self = this;
+  return function(file, next) {
+    self.union('files', file.path);
+    next();
+  };
+};
+
+/**
+ * Add a reporter function to the reporter with the given name.
+ *
+ * ```js
+ * app.reporter.add('basic', function() {
+ *   console.log(this.files);
+ * });
+ * ```
+ * @name reporter.add
+ * @param {String} `name` Name of the reporter
+ * @param {Function} `reporter` Function to run when this reporter is used.
+ * @return {Object} `this` to enable chaining
+ * @api public
+ */
+
+Reporter.prototype.add = function(name, reporter) {
+  this.set(['reporters', name], reporter);
+  return this;
+};
+
+/**
+ * Run a registered reporter function.
+ *
+ * ```js
+ * app.reporter.report('basic');
+ * //=> file1.js,file2.js,file3.js
+ * ```
+ * @name reporter.report
+ * @param  {String} `name` Name of the report to run.
+ * @return {Object} `this` to enable chaining
+ * @api public
+ */
+
+Reporter.prototype.report = function(name) {
+  var reporter = this.get(['reporters', name]);
+  if (typeof reporter !== 'function') {
+    throw new Error(`Unable to find reporter "${name}"`);
+  }
+  reporter.call(this, this);
+  return this;
+};
+
--- test.js
+++ test.js
@@ -2,27 +2,71 @@
 
 require('mocha');
 var assert = require('assert');
-var baseReporter = require('./');
+var Base = require('base');
+var reporter = require('./');
+var app;
 
 describe('base-reporter', function() {
-  it('should export a function', function() {
-    assert.equal(typeof baseReporter, 'function');
-  });
+  describe('plugin', function() {
+    beforeEach(function() {
+      app = new Base({isApp: true});
+    });
 
-  it('should export an object', function() {
-    assert(baseReporter);
-    assert.equal(typeof baseReporter, 'object');
-  });
+    it('should export a function', function() {
+      assert.equal(typeof reporter, 'function');
+    });
 
-  it('should throw an error when invalid args are passed', function(cb) {
-    try {
-      baseReporter();
-      cb(new Error('expected an error'));
-    } catch (err) {
-      assert(err);
-      assert.equal(err.message, 'expected first argument to be a string');
-      assert.equal(err.message, 'expected callback to be a function');
+    it('should only register the plugin once', function(cb) {
+      var count = 0;
+      app.on('plugin', function(name) {
+        if (name === 'base-reporter') {
+          count++;
+        }
+      });
+      app.use(reporter());
+      app.use(reporter());
+      app.use(reporter());
+      assert.equal(count, 1);
       cb();
-    }
+    });
+
+    it('should add a `reporter` object on the `app`', function() {
+      app.use(reporter());
+      assert.equal(typeof app.reporter, 'object');
+    });
+  });
+
+  describe('reporter', function() {
+    beforeEach(function() {
+      app = new Base({isApp: true});
+      app.use(reporter());
+    });
+
+    it('should add a reporter to the reporters cache', function() {
+      app.reporter.add('foo', function() {});
+      assert.equal(typeof app.reporter.reporters.foo, 'function');
+    });
+
+    it('should use a registered reporter', function() {
+      var count = 0;
+      app.reporter.add('foo', function() {
+        count++;
+      });
+
+      app.reporter.report('foo');
+      assert.equal(count, 1);
+    });
+
+    it('should throw an error when a reporter is not registered', function(cb) {
+      app.reporter.add('foo', function() {});
+      try {
+        app.reporter.report('bar');
+        cb(new Error('expected an error'));
+      } catch (err) {
+        assert(err);
+        assert.equal(err.message, 'Unable to find reporter "bar"');
+        cb();
+      }
+    });
   });
 });
